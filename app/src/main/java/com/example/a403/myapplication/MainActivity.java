cb608bebaf21c1f60d589e22ca67fd275628952e
서영균 <vault961@gmail.com>
2016-11-18 19:58:08 +0900

    프로젝트 생성및 레이아웃 세팅

--- app/src/main/java/com/example/a403/myapplication/MainActivity.java
+++ app/src/main/java/com/example/a403/myapplication/MainActivity.java
@@ -2,16 +2,26 @@ package com.example.a403.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutCompat;
+import android.support.v7.widget.ViewStubCompat;
+import android.text.Layout;
+import android.view.View;
 import android.widget.CheckBox;
 
 public class MainActivity extends AppCompatActivity {
     CheckBox c1;
+    View l1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        l1=(View) findViewById(R.id.linear);
+        setCheckBox();
+    }
+    void setCheckBox(){
         c1=(CheckBox)findViewById(R.id.checkBox);
+       if(c1.isChecked()){
 
+       }
     }
 }
